d307a1d3c19c6d34223f93dc73263cb9641a9b6e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-24 02:41:12 +0000

    Deploy jonthornton07/personal-website to github.com/jonthornton07/personal-website.git:gh-pages

---
+++ src/pages/profile/ProfileSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Header, List, Segment } from "semantic-ui-react";
+
+const ProfileSection = ({
+  timeFrame,
+  company,
+  location,
+  title,
+  highlights,
+}) => {
+  return (
+    <Segment basic compact>
+      <Header size="medium">
+        {title}
+        <Header.Subheader size="small">
+          {company} / {timeFrame}
+        </Header.Subheader>
+      </Header>
+      <List as="ul">
+        {highlights.map((highlight, index) => {
+          return (
+            <List.Item key={index} as="li">
+              {highlight}
+            </List.Item>
+          );
+        })}
+      </List>
+    </Segment>
+  );
+};
+
+export default ProfileSection;
