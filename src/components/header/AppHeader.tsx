f3f39c8a59be0fce6b78679bf6a18d4ec19d6973
jonthornton07 <jon.thornton07@gmail.com>
2023-07-09 16:01:09 -0500

    cleaning up some errors

--- src/components/header/AppHeader.tsx
+++ src/components/header/AppHeader.tsx
@@ -3,6 +3,9 @@ import type { Page } from "@/enums/pages";
 import { activePages } from "@/enums/pages"
 import Tab from "@/components/base/tab/Tab"
 import Banner from "./Banner"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 
 interface Props {
     activePage: Page
@@ -12,16 +15,23 @@ interface Props {
 const AppHeader: React.FC<Props> = ({activePage}) => {
     return (
         <Banner>
-            <div className="flex">
-                {activePages.map(page => {
-                    return(
-                        <Tab 
-                            key={page.name} 
-                            active={activePage === page} 
-                            link={page.route.toLowerCase()}
-                            text={page.name.toUpperCase()} />
-                    )
-                })}
+            <div className="grid grid-cols-2 gap-4">
+                <div className="flex bg-purple-400 place-content-center">
+                    {activePages.map(page => {
+                        return(
+                            <Tab 
+                                key={page.name} 
+                                active={activePage === page} 
+                                link={page.route.toLowerCase()}
+                                text={page.name.toUpperCase()} />
+                        )
+                    })}
+                </div>
+                <div className="flex flex-row-reverse bg-red-500 justify-items-end place-content-center">
+                    <FontAwesomeIcon icon={faLinkedinIn} size="xl" />
+                    <FontAwesomeIcon icon={faGithub} size="xl"  />
+                    <FontAwesomeIcon icon={faTwitter} size="xl"  />
+                </div>
             </div>
         </Banner>
     )
